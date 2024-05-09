37ea865abf6ed8791f2afd109ac2b7be7266dc32
Matias Arroyo <matiasarroyo684@gmail.com>
2024-05-09 16:20:40 -0300

    first commit

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -3,15 +3,17 @@ import { FaLinkedin, FaGithub } from 'react-icons/fa';
 
 const Footer = () => {
   return (
-    <footer className="w-full text-gray-900 p-4 flex flex-col items-center justify-center">
-      <span>© 2024 Matias Arroyo. All rights reserved.</span>
-      <div>
-        <a href="https://linkedin.com/in/matias-arroyo19" target="_blank" rel="noopener noreferrer" className="text-black p-2">
-          <FaLinkedin size={24} />
-        </a>
-        <a href="https://github.com/matiasarroyo1978/audio-test" target="_blank" rel="noopener noreferrer" className="text-black p-2">
-          <FaGithub size={24} />
-        </a>
+    <footer className="w-full bg-gray-100 text-gray-800 p-4 flex justify-center items-center">
+      <div className="flex justify-between items-center w-full max-w-4xl">
+        <span>© 2024 Matias Arroyo. All rights reserved.</span>
+        <div>
+          <a href="https://linkedin.com/in/matias-arroyo19" target="_blank" rel="noopener noreferrer" className="text-gray-800 p-2">
+            <FaLinkedin size={24} />
+          </a>
+          <a href="https://github.com/matiasarroyo1978/audio-test" target="_blank" rel="noopener noreferrer" className="text-gray-800 p-2">
+            <FaGithub size={24} />
+          </a>
+        </div>
       </div>
     </footer>
   );
